a9841545dbbd4fee1dc5fbf5a3c561dd1cf65a3f
michelelongo97 <m.longo7x@gmail.com>
2026-01-11 12:02:23 +0100

    feat: add back to home btn in PageNotFound

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,8 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 export default function PageNotFound() {
+  const navigate = useNavigate();
+
   return (
     <div className="notfound">
       <img src="/images/logos/notfound.jpeg" alt="Pagina non trovata" />
       <h1>Pagina non trovata</h1>
+      <button className="btn" onClick={() => navigate("/")}>
+        Torna in Home
+      </button>
     </div>
   );
 }
