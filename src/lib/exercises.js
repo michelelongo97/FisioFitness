6b182b7caf89300d412c3a9fde4b9085b974a4d2
michelelongo97 <m.longo7x@gmail.com>
2026-08-27 10:11:31 +0200

    feat: aggiunta nuovi esercizi

--- src/lib/exercises.js
+++ src/lib/exercises.js
@@ -1,13 +1,24 @@
 export const EXERCISES = [
-  {
-    key: "trazioni",
-    name: "Trazioni",
-    image: "/images/exercises/trazioni.jpg",
-  },
+  { key: "squat", name: "Squat", image: "/images/exercises/squat.jpg" },
   {
     key: "leg-press",
     name: "Leg Press",
     image: "/images/exercises/leg-press.jpg",
+  },
+  {
+    key: "stacco-bilanciere",
+    name: "Stacco Bilanciere",
+    image: "/images/exercises/stacco-bilanciere.jpg",
+  },
+  {
+    key: "affondo-camminata",
+    name: "Affondo Camminata",
+    image: "/images/exercises/affondo-camminata.jpg",
+  },
+  {
+    key: "panca-piana",
+    name: "Panca Piana",
+    image: "/images/exercises/panca-piana.jpg",
   },
   {
     key: "chest-press",
@@ -19,20 +30,26 @@ export const EXERCISES = [
     name: "Military Press",
     image: "/images/exercises/military-press.jpg",
   },
+
   {
-    key: "panca-piana",
-    name: "Panca Piana",
-    image: "/images/exercises/panca-piana.jpg",
+    key: "alazate-testa-manubri-panca",
+    name: "Alzate sulla Testa Manubri su Panca",
+    image: "/images/exercises/alzate-testa-manubri-panca.jpg",
   },
   {
-    key: "affondo-camminata",
-    name: "Affondo Camminata",
-    image: "/images/exercises/affondo-camminata.jpg",
+    key: "trazioni",
+    name: "Trazioni",
+    image: "/images/exercises/trazioni.jpg",
   },
-  { key: "squat", name: "Squat", image: "/images/exercises/squat.jpg" },
+
   {
-    key: "stacco-bilanciere",
-    name: "Stacco Bilanciere",
-    image: "/images/exercises/stacco-bilanciere.jpg",
+    key: "lat-machine-asta-dritta",
+    name: "Lat Machine Asta Dritta",
+    image: "/images/exercises/lat-machine-asta-dritta.jpg",
+  },
+  {
+    key: "push-down-corda",
+    name: "Push Down Corda",
+    image: "/images/exercises/push-down-corda.jpg",
   },
 ];
